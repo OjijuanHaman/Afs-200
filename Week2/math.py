f7f5b9d9dc5697fcfcf98768f3b684118181d12a
OjijuanHaman <ojijuan.haman@bryanuniversity.edu>
2021-12-12 14:55:21 -0600

    added week2 1st assignment

---
+++ Week2/math.py
@@ -0,0 +1,7 @@
+radiusCalc = int(input ("What the radius of the circle?"))
+square = 2
+pi = 3.14
+
+areaOfCircle = radiusCalc ** square * pi
+
+print ("Area of circle : ", radiusCalc ** square * pi )
---
+++ Week2/variable.py
@@ -0,0 +1,7 @@
+failed = "2"
+student1 = "John"
+student2 = "Eric"
+
+print ("Your son " + student1 + " is failing " + failed + " subjects.")
+print (student1 + ' will need to redo ' + failed + ' courses.')
+print(student2 + ' is doing well in Geography.')
